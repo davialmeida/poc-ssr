deea14866c639216da224ce06ae5b6940890edd5
Davi Almeida <davistrife@live.com>
2023-11-08 10:18:48 -0300

    fix: visualização

--- src/client/components/users.tsx
+++ src/client/components/users.tsx
@@ -6,6 +6,21 @@ export const Users: React.FC = () => {
     const { data } = useQuery({ queryKey: ['users'], queryFn: UsersService.get})
 
     return (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        <table>
+            <thead>
+                <th>Login</th>
+                <th>Url</th>
+                <th>Type</th>
+            </thead>
+            <tbody>
+                {data?.map(user => (
+                    <tr key={user.id}>
+                        <td>{user.login}</td>
+                        <td>{user.url}</td>
+                        <td>{user.type}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     )
 }
